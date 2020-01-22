4bdca277b12d9cf4b99b3e317113649896d4f098
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-22 01:37:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,9 @@
-def introduction_with_language(name,"Ember.js")
+def introduction_with_language(name,file)
+  puts "Hi, my name is #{name} and I am learning to program in #{file}."
 end
+
+introduction_with_language("Dan","Ember.js")
+
 
 
 
